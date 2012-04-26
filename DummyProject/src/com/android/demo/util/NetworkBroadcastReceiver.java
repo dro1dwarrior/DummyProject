8e9c88e3f401cf03478364f9c1abcf9b85f5b463
Jiten Gandhi <jitengandhi@gmail.com>
2012-04-26 16:52:44 +0530

    network changes

--- DummyProject/src/com/android/demo/util/NetworkBroadcastReceiver.java
+++ DummyProject/src/com/android/demo/util/NetworkBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.android.demo.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class NetworkBroadcastReceiver extends BroadcastReceiver
 {
@@ -10,6 +11,7 @@ public class NetworkBroadcastReceiver extends BroadcastReceiver
     @Override
     public void onReceive( Context argContext, Intent argIntent )
     {
+        Log.d( "NetworkBroadcastReceiver-onReceive()", "NETWORK STATE CHANGE *********************" );
         Util.setNetworkStatus();
     }
 }
